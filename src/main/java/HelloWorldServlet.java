9166cdbee74620e8382301c2183b8651696c2050
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-04-27 16:26:51 -0500

    Adding name viewed function

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
@@ -6,7 +7,8 @@ import javax.servlet.http.*;
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
 
-    int count = 0;
+    private int count = 0;
+    private ArrayList<String> names = new ArrayList<>();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
@@ -14,6 +16,7 @@ public class HelloWorldServlet extends HttpServlet {
 
         String name = request.getParameter("name");
         if (name != null && !name.isEmpty()) {
+            names.add(name);
             out.println("<h1>Hello " + name + "!</h1>");
         } else {
             out.println("<h1>Hello, World!</h1>");
@@ -21,13 +24,17 @@ public class HelloWorldServlet extends HttpServlet {
 
         count++;
         out.println("<h1>Page views: " + count + "</h1>");
+        out.println("<h2>Name viewed: " + names + "</h2><br>");
 
 
         String reset = request.getParameter("reset");
         out.println("<h2>Reset count: /hello?reset=true");
 
+
         if (reset != null && reset.equals("true")) {
             count = 0;
+            names.clear();
+
         }
     }
 }
